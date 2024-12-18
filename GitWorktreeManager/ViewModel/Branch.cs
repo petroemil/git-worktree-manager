f4ab1ba4c0482cc9d33c80440de23b28938b6b94
Emil Petro <empetro@microsoft.com>
2024-12-18 13:53:03 -0800

    Implementing equality operators on branch VM classes

--- GitWorktreeManager/ViewModel/Branch.cs
+++ GitWorktreeManager/ViewModel/Branch.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Input;
 
-internal abstract class Branch
+internal abstract class Branch : IEquatable<Branch?>
 {
     public required string Name { get; init; }
     public string DisplayName => Name.Replace("/", " / ");
@@ -19,9 +19,27 @@ internal abstract class Branch
 
     public string AheadBehindCommitsLabel => $"{Ahead} outgoing, {Behind} incoming commits";
     public string AheadBehindCommitsDisplay => $"{Ahead}/{Behind}";
+
+    public override bool Equals(object? obj) 
+        => Equals(obj as Branch);
+
+    public bool Equals(Branch? other) 
+        => other is not null
+            && this.Name == other.Name
+            && this.Ahead == other.Ahead
+            && this.Behind == other.Behind;
+
+    public override int GetHashCode()
+        => HashCode.Combine(Name, Ahead, Behind);
+
+    public static bool operator ==(Branch? left, Branch? right)
+        => EqualityComparer<Branch>.Default.Equals(left, right);
+
+    public static bool operator !=(Branch? left, Branch? right)
+        => !(left == right);
 }
 
-internal abstract class BranchWithWorktree : Branch
+internal abstract class BranchWithWorktree : Branch, IEquatable<BranchWithWorktree?>
 {
     public required string Path { get; init; }
 
@@ -36,20 +54,41 @@ internal abstract class BranchWithWorktree : Branch
 
     public string OpenVisualStudioLabel => "Open in Visual Studio";
     public required ICommand OpenVisualStudioCommand { get; init; }
+
+    public override bool Equals(object? obj)
+        => this.Equals(obj as BranchWithWorktree);
+
+    public bool Equals(BranchWithWorktree? other) 
+        => base.Equals(other) && this.Path == other.Path;
+
+    public override int GetHashCode()
+        => HashCode.Combine(base.GetHashCode(), this.Path);
+
+    public static bool operator ==(BranchWithWorktree? left, BranchWithWorktree? right) 
+        => EqualityComparer<BranchWithWorktree>.Default.Equals(left, right);
+
+    public static bool operator !=(BranchWithWorktree? left, BranchWithWorktree? right) 
+        => !(left == right);
 }
 
 internal sealed class HeadBranchWithWorktree : BranchWithWorktree, IEquatable<HeadBranchWithWorktree?>
 {
     public override string Label => "HEAD branch";
 
-    public override bool Equals(object? obj) => Equals(obj as HeadBranchWithWorktree);
-    public bool Equals(HeadBranchWithWorktree? other) 
-        => other is not null
-        && this.Name == other.Name
-        && this.Path == other.Path;
-    public override int GetHashCode() => HashCode.Combine(this.Name, this.Path);
-    public static bool operator ==(HeadBranchWithWorktree? left, HeadBranchWithWorktree? right) => EqualityComparer<HeadBranchWithWorktree>.Default.Equals(left, right);
-    public static bool operator !=(HeadBranchWithWorktree? left, HeadBranchWithWorktree? right) => !(left == right);
+    public override bool Equals(object? obj)
+        => this.Equals(obj as HeadBranchWithWorktree);
+
+    public bool Equals(HeadBranchWithWorktree? other)
+        => base.Equals(other);
+
+    public override int GetHashCode() 
+        => base.GetHashCode();
+
+    public static bool operator ==(HeadBranchWithWorktree? left, HeadBranchWithWorktree? right)
+        => EqualityComparer<HeadBranchWithWorktree>.Default.Equals(left, right);
+
+    public static bool operator !=(HeadBranchWithWorktree? left, HeadBranchWithWorktree? right)
+        => !(left == right);
 }
 
 internal sealed class LocalBranchWithWorktree : BranchWithWorktree, IEquatable<LocalBranchWithWorktree?>
@@ -59,44 +98,79 @@ internal sealed class LocalBranchWithWorktree : BranchWithWorktree, IEquatable<L
     public string RemoveLabel => "Remove worktree";
     public required ICommand RemoveCommand { get; init; }
 
-    public override bool Equals(object? obj) => Equals(obj as LocalBranchWithWorktree);
-    public bool Equals(LocalBranchWithWorktree? other) 
-        => other is not null
-        && this.Name == other.Name
-        && this.Path == other.Path;
-    public override int GetHashCode() => HashCode.Combine(this.Name, this.Path);
-    public static bool operator ==(LocalBranchWithWorktree? left, LocalBranchWithWorktree? right) => EqualityComparer<LocalBranchWithWorktree>.Default.Equals(left, right);
-    public static bool operator !=(LocalBranchWithWorktree? left, LocalBranchWithWorktree? right) => !(left == right);
+    public override bool Equals(object? obj)
+        => this.Equals(obj as LocalBranchWithWorktree);
+
+    public bool Equals(LocalBranchWithWorktree? other)
+        => base.Equals(other);
+
+    public override int GetHashCode() 
+        => base.GetHashCode();
+
+    public static bool operator ==(LocalBranchWithWorktree? left, LocalBranchWithWorktree? right)
+        => EqualityComparer<LocalBranchWithWorktree>.Default.Equals(left, right);
+
+    public static bool operator !=(LocalBranchWithWorktree? left, LocalBranchWithWorktree? right)
+        => !(left == right);
 }
 
-internal abstract class BranchWithoutWorktree : Branch
+internal abstract class BranchWithoutWorktree : Branch, IEquatable<BranchWithoutWorktree?>
 {
     public string CreateWorktreeForBranchLabel => $"Set up worktree for '{DisplayName}'";
     public required ICommand CreateWorktreeForBranchCommand { get; init; }
+
+    public override bool Equals(object? obj) 
+        => this.Equals(obj as BranchWithoutWorktree);
+
+    public bool Equals(BranchWithoutWorktree? other)
+        => base.Equals(other);
+
+    public override int GetHashCode() 
+        => base.GetHashCode();
+
+    public static bool operator ==(BranchWithoutWorktree? left, BranchWithoutWorktree? right) 
+        => EqualityComparer<BranchWithoutWorktree>.Default.Equals(left, right);
+
+    public static bool operator !=(BranchWithoutWorktree? left, BranchWithoutWorktree? right)
+        => !(left == right);
 }
 
 internal sealed class LocalBranchWithoutWorktree : BranchWithoutWorktree, IEquatable<LocalBranchWithoutWorktree?>
 {
     public override string Label => "Local branch";
 
-    public override bool Equals(object? obj) => Equals(obj as LocalBranchWithoutWorktree);
-    public bool Equals(LocalBranchWithoutWorktree? other) 
-        => other is not null 
-        && this.Name == other.Name;
-    public override int GetHashCode() => HashCode.Combine(this.Name);
-    public static bool operator ==(LocalBranchWithoutWorktree? left, LocalBranchWithoutWorktree? right) => EqualityComparer<LocalBranchWithoutWorktree>.Default.Equals(left, right);
-    public static bool operator !=(LocalBranchWithoutWorktree? left, LocalBranchWithoutWorktree? right) => !(left == right);
+    public override bool Equals(object? obj) 
+        => this.Equals(obj as LocalBranchWithoutWorktree);
+
+    public bool Equals(LocalBranchWithoutWorktree? other)
+        => base.Equals(other);
+
+    public override int GetHashCode() 
+        => base.GetHashCode();
+
+    public static bool operator ==(LocalBranchWithoutWorktree? left, LocalBranchWithoutWorktree? right) 
+        => EqualityComparer<LocalBranchWithoutWorktree>.Default.Equals(left, right);
+
+    public static bool operator !=(LocalBranchWithoutWorktree? left, LocalBranchWithoutWorktree? right)
+        => !(left == right);
 }
 
 internal sealed class RemoteBranchWithoutWorktree : BranchWithoutWorktree, IEquatable<RemoteBranchWithoutWorktree?>
 {
     public override string Label => "Remote branch";
 
-    public override bool Equals(object? obj) => Equals(obj as RemoteBranchWithoutWorktree);
-    public bool Equals(RemoteBranchWithoutWorktree? other) 
-        => other is not null 
-        && this.Name == other.Name;
-    public override int GetHashCode() => HashCode.Combine(this.Name);
-    public static bool operator ==(RemoteBranchWithoutWorktree? left, RemoteBranchWithoutWorktree? right) => EqualityComparer<RemoteBranchWithoutWorktree>.Default.Equals(left, right);
-    public static bool operator !=(RemoteBranchWithoutWorktree? left, RemoteBranchWithoutWorktree? right) => !(left == right);
+    public override bool Equals(object? obj) 
+        => this.Equals(obj as RemoteBranchWithoutWorktree);
+
+    public bool Equals(RemoteBranchWithoutWorktree? other)
+        => base.Equals(other);
+
+    public override int GetHashCode() 
+        => base.GetHashCode();
+
+    public static bool operator ==(RemoteBranchWithoutWorktree? left, RemoteBranchWithoutWorktree? right) 
+        => EqualityComparer<RemoteBranchWithoutWorktree>.Default.Equals(left, right);
+
+    public static bool operator !=(RemoteBranchWithoutWorktree? left, RemoteBranchWithoutWorktree? right)
+        => !(left == right);
 }
